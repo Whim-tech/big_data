5bed33b29a1209f18e0be026b8465f8e55dfc809
Whim <108950116+Whim-tech@users.noreply.github.com>
2023-11-22 13:34:59 +0600

    json renaming

--- second_task/json_downloader.py
+++ second_task/json_downloader.py
@@ -1,16 +1,23 @@
 from mrjob.job import MRJob
 
 from urllib.request import urlopen
-import uuid
-import json
+import re
 
 output_dir = "E:/ucheba/bi_data/second_task/json"
+
+url_regex = r"https://rasp\.omgtu\.ru/api/schedule/person/(\d+)\?start=(\d\d\d\d\.\d\d\.\d\d)&finish=(\d\d\d\d\.\d\d\.\d\d)&lng=1"
+
+def parse_url(url):
+    result = re.search(url_regex, url)
+
+    return result.group(1), result.group(2).replace(".", "-"), result.group(3).replace(".", "-")
 
 class JsonDownloader(MRJob):
 
     def mapper(self, _, line):
+        id, start, end = parse_url(line)
         response = urlopen(line)
-        with open(f"{output_dir}/{uuid.uuid1()}.json", "wb") as file:
+        with open(f"{output_dir}/{id}_{start}_{end}.json", "wb") as file:
             file.write(response.read())
         yield  None, None
 
