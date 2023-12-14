d03b7a46badf6c6bd29dc1075232a2e0b82661a0
Whim <108950116+Whim-tech@users.noreply.github.com>
2023-12-14 21:39:11 +0600

    final implementation

--- second_task/json_downloader.py
+++ second_task/json_downloader.py
@@ -1,25 +1,44 @@
 from mrjob.job import MRJob
 
 from urllib.request import urlopen
-import re
 
-output_dir = "E:/ucheba/bi_data/second_task/json"
+from datetime import datetime, timedelta
 
-url_regex = r"https://rasp\.omgtu\.ru/api/schedule/person/(\d+)\?start=(\d\d\d\d\.\d\d\.\d\d)&finish=(\d\d\d\d\.\d\d\.\d\d)&lng=1"
-
-def parse_url(url):
-    result = re.search(url_regex, url)
-
-    return result.group(1), result.group(2).replace(".", "-"), result.group(3).replace(".", "-")
+output_dir = "/home/whim/univer/seventh_term/big_data/labas/second_task/json"
 
 class JsonDownloader(MRJob):
-
-    def mapper(self, _, line):
-        id, start, end = parse_url(line)
-        response = urlopen(line)
-        with open(f"{output_dir}/{id}_{start}_{end}.json", "wb") as file:
+    
+    def configure_args(self):
+        super(JsonDownloader, self).configure_args()
+        self.add_passthru_arg( "--type", default="person", choices=["person", "group", "auditorium"])
+        self.add_passthru_arg( "--start_date",  type=str, required=True)
+        self.add_passthru_arg( "--end_date",    type=str, required=True)
+        
+    def mapper(self, _, id):
+        start_date = datetime.strptime(self.options.start_date, "%Y.%m.%d")
+        end_date   = datetime.strptime(self.options.end_date, "%Y.%m.%d")
+        api_type =  self.options.type
+        
+        date = start_date
+        while (date <= end_date):
+            
+            start = date.strftime("%Y.%m.%d")
+            end   = (date + timedelta(days=6)).strftime("%Y.%m.%d")
+            
+            url = f"https://rasp.omgtu.ru/api/schedule/{api_type}/{id}?start={start}&finish={end}&lng=1"
+            
+            yield (url, start, end, id) , None
+            
+            date = date + timedelta(days=7)
+            
+        
+        
+    def reducer(self, data, _):
+        
+        response = urlopen(data[0])
+        
+        with open(f"{output_dir}/{self.options.type}/{data[3]}_{data[1]}_{data[2]}.json", "wb+") as file:
             file.write(response.read())
-        yield  None, None
 
 
 if __name__ == '__main__':
